2b381fa623525dd47562b22e137dc5eecf2fa247
cedrick <cedrickmacatangay@gmail.com>
2012-03-31 00:46:59 +0800

    logout

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+	session_start();
+     if(!session_is_registered(username))
+		{
+ 		 header("location:index.php");
+		}
+?>
+
+<?php
+
+      
+	    include ("includes/rec_class.php");
+			$check= new Check();
+			$check->activity_of($user);
+	    	$check->logout();
+	
+?>
+
+
