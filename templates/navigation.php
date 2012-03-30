55d36bdd45a4263ced63ed41c7f01521ccc8ee4a
cedrick <cedrickmacatangay@gmail.com>
2012-03-31 00:44:03 +0800

    templates

---
+++ templates/navigation.php
@@ -0,0 +1,40 @@
+<style type="text/css">
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+<div class="menu"> 
+	<a href="admin_out.php">     
+		<font color="#FFFBF0"face="Arial Rounded MT Bold" size="3">
+			Sign Out
+		</font>
+	</a>
+	&nbsp;&nbsp;
+	<a href="register.php">
+	        <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+	           Sign Up
+	        </font>
+	</a>
+	&nbsp;&nbsp;
+	<a href="admin_portal.php">
+          <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+            View User
+          </font>
+     </a>
+     &nbsp;&nbsp;
+      <a href="recent_logs.php">
+         <font color="#FFFBF0" face="Arial Rounded MT Bold" size="3">
+           View Logs
+         </font>
+      </a>
+					
+</div>
